a3daa1bf173c4540274ae178a41ea77d73c914d8
Sambo Chea <sombochea100@gmail.com>
2018-11-22 21:54:13 +0700

    add identity

---
+++ OkooneBlogger/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OkooneBlogger.Models;
+
+[assembly: HostingStartup(typeof(OkooneBlogger.Areas.Identity.IdentityHostingStartup))]
+namespace OkooneBlogger.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<OkooneBloggerContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("OkooneBloggerContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<OkooneBloggerContext>();
+            });
+        }
+    }
+}
